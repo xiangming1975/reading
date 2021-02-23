0578e09cba2a07afc8c7e85118de517dcca93d23
XiangMing@CM <xiangming@live.com>
2021-02-23 09:23:42 +0800

    更新sphinx的mermaid插件。

--- source/conf.py
+++ source/conf.py
@@ -41,6 +41,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.mermaid',
 #    'sphinxcontrib.fulltoc', 
     'recommonmark'
 ]
